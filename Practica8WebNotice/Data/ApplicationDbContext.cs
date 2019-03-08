7f1ec6d05d5c2ac86f634ff8cb495efa5ada1500
LauraDamian <yareth_2793@hotmail.com>
2019-03-08 10:59:21 -0600

    Agregar archivos de proyecto.

---
+++ Practica8WebNotice/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Practica8WebNotice.Models;
+
+namespace Practica8WebNotice.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Categoria>  Categorias { get; set; }
+        public DbSet<Noticia> Noticias { get; set; }
+    }
+}
---
+++ Practica8WebNotice/Models/Noticia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica8WebNotice.Models
+{
+    public class Noticia
+    {
+        public int NoticiaID { get; set; }
+   
+        [Required]
+        public string Titulo { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Contenido { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int CategoriaID { get; set; }
+        public virtual Categoria Categoria{ get; set; }
+    }
+}
